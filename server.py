a49c6bb7c6796ed227398e3347c94b0b2b9383bb
Ikarulus <github.ikarulus@zer.one>
2017-06-11 10:05:54 +0200

    got it workingw

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ from flask import request
 import sqlite3
 import os
 from flask import jsonify
+import json
 
 if os.path.exists("citycleanup.db")==False:
     conn = sqlite3.connect('citycleanup.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE Verschmutzung
     (
-    ID INTEGER NOT NULL,
+    id INTEGER NOT NULL,
     LAENGE FLOAT,
     BREITE FLOAT,
     VERSCHMUTZUNGSGRAD INTEGER,
@@ -20,21 +21,39 @@ else:
     conn = sqlite3.connect('citycleanup.db')
     c = conn.cursor()
 
+
+c.execute("SELECT * FROM Verschmutzung")
+print(c.fetchall())
+
 app = Flask(__name__)
 
-#@app.route('/api/submit', methods=['POST'])
-#def submit():
-#    c.execute("insert into user(username, password) values(?, ?)", (request.form['laenge'], request.form['breite'], request.form['verschmutzungsgrad'], request.form['verschmurtungsgrad']))
-#    return 'success', 200
+@app.route('/api/submit', methods=['POST'])
+#TODO: Begrenzungen für Anfragen
+def submit():
+    c.execute('insert into Verschmutzung (LAENGE, BREITE, VERSCHMUTZUNGSGRAD, BESCHREIBUNG) values(?, ?, ?, ?)', (request.form['laenge'], request.form['breite'], request.form['verschmutzungsgrad'], request.form['beschreibung']))
+    conn.commit()
+    print(request.form)
+    return 'success', 200
 
 @app.route('/api/get_markers', methods=['GET'])
 def get_markers():
+#TODO: Nur in der Nähe anzeigen
     c.execute("SELECT * FROM Verschmutzung;")
-    out=''
-    test=["wfewfewq", "fffaf"]
-    for table_row in c:
-        out=out+jsonify(id=row[0], laenge=row[1], breite=row[2], verschmutzungsgrad=row[3], beschreibung=row[4]+","
-    return out, 200
+    #Geklaut von Stackoverflow
+    rows = [x for x in c]
+    cols = [x[0] for x in c.description]
+    markers = []
+    marker = {}
+    for row in rows:
+        marker = {}
+        for prop, val in zip(cols, row):
+            marker[prop] = val
+        markers.append(marker)
+
+    # Create a string representation of your array of songs.
+    print(markers)
+    return json.dumps(markers), 200
+
 
 if __name__ == "__main__":
     app.run()
